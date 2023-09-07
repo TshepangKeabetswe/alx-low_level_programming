aa8b49d3af22e333b18780ba74d59a9185be487b
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-07 16:38:16 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a printf program that prints strings
+ * Return: 0 if successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
