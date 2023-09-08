9e2f9c2eedcbc1b5517dba8694d1f2f367ce610d
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-07 17:31:33 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints a line of string
+ * Return: 1 if successful
+ */
+int main(void)
+{
+fputs("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (l);
+}
