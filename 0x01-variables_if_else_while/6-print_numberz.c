bd7ca5ca9db3ed963129530ceb290b762ebeff21
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-11 16:15:57 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a progarm that print numbers
+ * Return
+ */
+int main(void)
+{
+	int b;
+
+        b = 0;
+	for (b = 0; b < 10; b++)
+		putchar(b + '0');
+	putchar('\n');
+	return (0);
+}
