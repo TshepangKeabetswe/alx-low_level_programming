fc8930f6ac143fd7ff3008a77176662288f4eee3
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-11 15:14:06 -0700

    'print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabet
+ * Return: 0 success
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+		putchar(b);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
