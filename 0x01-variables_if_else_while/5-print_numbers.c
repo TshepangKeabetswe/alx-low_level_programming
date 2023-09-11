813c1af811d91762da3a66ebacf974d9b0d2b3c0
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-11 15:47:04 -0700

    rint numbers
    "

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints single digit numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 10; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
