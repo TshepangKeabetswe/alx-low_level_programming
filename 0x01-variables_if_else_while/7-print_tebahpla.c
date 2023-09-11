7e73553a0a6fa9e5b7813a2bbd303d7e9f101dac
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-11 16:47:11 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabet
+ * Return: 0 successs
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'z'; b < 'a'; b--)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
