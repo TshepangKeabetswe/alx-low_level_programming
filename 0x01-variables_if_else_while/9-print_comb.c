3316a1443bfd1bf03e0548d54dd00aa8b64300bf
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-11 17:42:14 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+		putchar(b + '0');
+	if (b != 9)
+		putchar(',');
+	putchar(' ');
+	return (0);
+}
