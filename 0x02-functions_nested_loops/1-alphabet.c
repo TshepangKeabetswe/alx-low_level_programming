c9a4c662a384b2d1043e126db060f58b49da2148
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-12 16:06:36 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet : this is the function of the task
+ * print_alphabet - this is the function of the task
  * Return: return 0
  */
 void print_alphabet(void)
