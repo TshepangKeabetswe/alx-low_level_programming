eb0efd6f6f93c908948c501f7b4166e111d77df8
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-09-21 16:23:07 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,6 +3,7 @@
  * _strcat - concatenates two strings
  * @dest:input value
  * @src: input value
+ *
  * Return: void
  */
 
@@ -12,7 +13,7 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
