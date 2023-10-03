12fdf12a3dc366c6ef2eee2e28385dba4f052f63
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-10-03 04:52:30 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check if a letter is upper
+ * @c: The number to be checked
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'Á' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
