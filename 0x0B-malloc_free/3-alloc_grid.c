f104eca69e93c3f602a69780227a209fbadc1d25
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-10-05 16:42:37 -0700

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,18 +10,18 @@
 int **alloc_grid(int width, int height)
 {
 	int **mee;
-	int x, y:
+	int x, y;
 
 		if (width <= 0 || height <= 0)
 			return (NULL);
 	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
 		return (NULL);
-	
+
 	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
-		
+
 		if (mee[x] == NULL)
 		{
 
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	FOR (x = 0; x < height; x++)
+	for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
