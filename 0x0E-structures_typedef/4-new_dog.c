7f01b16a304b3e0995a06e7bbc4190d8c583e97c
TshepangKeabetswe <tshepangkeabetswe44@gmail.com>
2023-10-25 05:04:36 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,23 +13,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1, len2;
-
-	len1 = strlen(name);
-	len2 = strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
